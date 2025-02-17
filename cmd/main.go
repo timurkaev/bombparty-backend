6f59a5127d6fa4f3727bcd3c4f82f287bb4a6485
Ismail_Timurkaev <timurkaev.Is@gmail.com>
2025-02-17 22:07:09 +0300

    added broadcast

--- cmd/main.go
+++ cmd/main.go
@@ -4,34 +4,67 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
+// Хранилище подключений
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan string)
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
+var mutex = &sync.Mutex{}
 
-func handleConnetions(w http.ResponseWriter, r *http.Request) {
+func handleConnections(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("Ошиька подключения", err)
+		log.Println("Ошибка подключения:", err)
 		return
 	}
 	defer conn.Close()
+
+	// Добавляем клиента в список
+	mutex.Lock()
+	clients[conn] = true
+	mutex.Unlock()
+
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
-			log.Println("Ошибка чтения", err)
+			log.Println("Ошибка чтения websocket:", err)
+			mutex.Lock()
+			delete(clients, conn)
+			mutex.Unlock()
 			break
 		}
 		log.Printf("Получено сообщение: %s", msg)
-		conn.WriteMessage(websocket.TextMessage, []byte("Hello from server"))
+		broadcast <- string(msg) // Отправляем сообщение всем
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		mutex.Lock()
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, []byte(msg))
+			if err != nil {
+				log.Println("Ошибка отправки сообщения:", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+		mutex.Unlock()
 	}
 }
 
 func main() {
-	http.HandleFunc("/ws", handleConnetions)
-	fmt.Println("Сервер запущен на  :8080")
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	fmt.Println("🚀 Сервер запущен на :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
